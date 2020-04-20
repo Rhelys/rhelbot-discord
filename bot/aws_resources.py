f0d720172d637141ef27e333241daa8b56bc4fea
Rhelys <qlverfaillie@gmail.com>
2020-04-20 11:46:57 -0700

    Updating directory, converting to a Bot class, and fixing formatting

---
+++ bot/aws_resources.py
@@ -0,0 +1,52 @@
+# aws_resources.py
+
+import boto3
+import json
+import decimal
+import socket
+
+from boto3.dynamodb.conditions import Key, Attr
+from botocore.exceptions import ClientError
+
+
+dynamodb = boto3.resource("dynamodb", region_name='us-west-2', endpoint_url="http://localhost:8000")
+whitelist_table = dynamodb.Table('rhelbot-discord')
+
+
+def fetch_bot_token():
+    local_hostname = socket.gethostname()
+    if local_hostname == 'Din':
+        boto_session = boto3.Session(profile_name='rhelbot')
+        s3_token = boto_session.client('s3')
+
+    else:
+        s3_token = boto3.client('s3')
+
+    s3_token.download_file('rhelbot-discord', "rhelbot_token.txt", "rhelbot_token.txt")
+
+
+def fetch_server_welcome(server_id):
+    try:
+        ddb_response = whitelist_table.get_item(
+            Key={
+                'server_id': server_id
+            }
+        )
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+        return 'This server has not been onboarded to the bot config yet'
+    else:
+        server_entry = ddb_response['Item']
+
+
+# Helper class to convert a DynamoDB item to JSON.
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            if o % 1 > 0:
+                return float(o)
+            else:
+                return int(o)
+        return super(DecimalEncoder, self).default(o)
+
+
---
+++ bot/bot.py
@@ -0,0 +1,36 @@
+# bot.py
+import discord
+from bot.bot import aws_resources as aws
+from discord.ext import commands
+
+# Connecting to Discord
+discord_client = discord.Client()
+
+rhelbot = commands.Bot(command_prefix='!rhel ')
+
+
+# Bot startup information
+@rhelbot.event
+async def on_ready(self):
+    print(f'Connected to Discord!\n\n')
+    print(f'{rhelbot.user} is connected to the following Discord servers:\n')
+    for guild in rhelbot.guilds:
+        print(f'{guild.name} (id: {guild.id})\n')
+
+
+# Per-server welcome messages
+@rhelbot.event
+async def on_member_join(member):
+    welcome_message = aws.fetch_server_welcome(member.guild.id)
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to {member.guild.name}!\n\n'
+        f'{welcome_message}'
+    )
+
+
+# Starting the bot up
+aws.fetch_bot_token()
+bot_token_file = open("rhelbot_token.txt", "r")
+bot_token = bot_token_file.read()
+rhelbot.run(bot_token)
